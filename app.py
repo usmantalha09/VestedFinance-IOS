ee9859b36b4e44b8269c2aa1845ec4118f73c611
usmantalha09 <usmantalha09@gmail.com>
2023-02-20 11:24:58 +0400

    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 
-for i in range(365, 565):  # Loop over 365 days (1 year)
+for i in range(730, 1065):  # Loop over 365 days (1 year)
     for j in range(0, randint(1, 8)):  # Random commits per day
         d = str(i) + ' days ago'  # Date format
         with open('file_new.txt', 'a') as file:  # Open file.txt in append mode
